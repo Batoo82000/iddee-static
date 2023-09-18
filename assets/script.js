9ef5b2a708fd17fdc69643427ed44061ef204ac8
unknown <batoo82000@gmail.com>
2023-09-18 16:33:06 +0200

    mise en place de la navbar et du footer, ainsi que de la page d'accueil

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+"use strict";
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".ul-navbar");
+const mobileLinks = document.querySelectorAll(".ul-navbar li a");
+burger.addEventListener("click", function () {
+  menu.classList.toggle("show");
+});
+
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    // Vérifiez si le menu est visible (la classe 'show' est présente)
+    if (menu.classList.contains("show")) {
+      // Masquez le menu en supprimant la classe 'show'
+      menu.classList.remove("show");
+    }
+  });
+});
+document.addEventListener("click", function (event) {
+  // Vérifiez si l'élément cliqué est en dehors du menu et du bouton burger
+  if (!menu.contains(event.target) && !burger.contains(event.target)) {
+    // Si c'est le cas, supprimez la classe 'show' du menu
+    menu.classList.remove("show");
+  }
+});
